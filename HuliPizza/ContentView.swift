81d00a498e44d07240cf89709126327b0a456bfd
Thirawat Prasert <tpr@manaosoftware.com>
2023-05-02 15:14:26 +0700

    Initial Commit

---
+++ HuliPizza/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  HuliPizza
+//
+//  Created by Thirawat First Prasert on 2/5/2566 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HuliPizza/HuliPizzaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HuliPizzaApp.swift
+//  HuliPizza
+//
+//  Created by Thirawat First Prasert on 2/5/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct HuliPizzaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
